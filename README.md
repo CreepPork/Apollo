af5836ec50c24de7c08d838da028a9bc85c7e218
Ralfs <creepporkstudios@gmail.com>
2018-07-10 22:30:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Apollo
+
+## Discord Bot to display Arma 3 server info
+The bot provides displays info about the:
+- Server title
+- Steam link to connect
+- Player count
+- Game mode
+- Map
+- Password protected
+
+
+## Requirements
+- Node >= 8.11.3
+- NPM >= 5.6.0
+
+
+## Setup
+- Get the code
+- `npm install`
+- `cp .env.example .env`
+- Configure the `.env` file
+- `node bot.js`
+- Good to go!
+
+
+## Available commands
+- `!update` to quickly get info about the server status.
---
+++ bot.js
@@ -0,0 +1,120 @@
+// Create a instance of the Discord client (bot)
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Load up the .env file variables (all changes to .env require bot restart!)
+require('dotenv').config();
+
+// Require GameDig - allows to get Server Query data.
+//  https://github.com/sonicsnes/node-gamedig
+const Gamedig = require('gamedig');
+
+/**
+ * Gets data from the Server Query and displays appropriate message as an activity, and calls createMessage().
+ *
+ */
+function updateInfo()
+{
+    Gamedig.query({
+        type: process.env.GAME_TYPE,
+        host: process.env.GAME_IP,
+        port: process.env.GAME_PORT,
+        port_query: process.env.GAME_QUERY_PORT
+    }).then(data => {
+        
+        client.user.setActivity(`Spēlētāji ${data.players.length}/${data.maxplayers} | ${data.map}`, 'PLAYING');
+        
+        createMessage(data, 'ok');
+    }).catch(error => {
+        client.user.setActivity('Vee! Voo! Serveris neatbild!', 'PLAYING');
+        
+        createMessage({}, 'error');
+        
+        console.error(error);
+    });
+}
+
+/**
+ * Creates the actual message in a Discord channel.
+ *
+ * @param {Object} [data={}] Server query from GameDig.
+ * @param {string} [status='ok'] Type of a message to return
+ */
+function createMessage(data = {}, status = 'ok')
+{
+    const channel = process.env.DISCORD_CHANNEL_ID;
+    
+    const statuses = {
+        ok: process.env.STATUS_COLOR_OK,
+        error: process.env.STATUS_COLOR_ERROR
+    };
+    
+    if (status === 'ok')
+    {
+        status = statuses.ok;
+        
+        client.channels.get(channel).send({
+            embed:
+            {
+                color: status,
+                author:
+                {
+                    name: data.name,
+                    icon_url: client.user.avatarURL
+                },
+                title: `steam://connect/${data.query.host}:${data.query.port_query}`,
+                fields:
+                [
+                    {
+                        name: 'Spēlētāji',
+                        value: `${data.players.length} no ${data.maxplayers}`
+                    },
+                    {
+                        name: 'Misijas tips',
+                        value: data.raw.game
+                    },
+                    {
+                        name: 'Karte',
+                        value: data.map
+                    },
+                    {
+                        name: 'Parole',
+                        value: data.password ? 'Ir' : 'Nav'
+                    },
+                ],
+            }
+        });
+    }
+    else
+    {
+        status = statuses.error;
+        
+        client.channels.get(channel).send({
+            embed:
+            {
+                color: status,
+                author:
+                {
+                    name: 'Serveris neatbild!',
+                    icon_url: client.user.avatarURL
+                },
+                description: 'Serveris neatbild, lūdzu salabojiet admini!'
+            }
+        });
+    }
+}
+
+client.on('ready', () => {
+    updateInfo();
+});
+
+client.on('message', message => {
+    if (message.content === '!update')
+    {
+        message.channel.send('Atjaunoju info!');
+        
+        updateInfo();
+    }
+});
+
+client.login(process.env.SECRET);
